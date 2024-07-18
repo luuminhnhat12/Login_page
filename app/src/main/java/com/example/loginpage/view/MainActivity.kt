37f82a0c8061da77914c04583191df358b2c85d4
luuminhnhat12 <nguoidep433@gmail.com>
2024-07-18 19:25:56 +0700

    state

--- app/src/main/java/com/example/loginpage/view/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/view/MainActivity.kt
@@ -13,6 +13,8 @@ import com.example.loginpage.databinding.ActivityMainBinding
 import com.example.loginpage.model.User
 import com.example.loginpage.viewmodel.UserViewModel
 import android.util.Log
+import androidx.lifecycle.Observer
+
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var binding: ActivityMainBinding
     lateinit var userViewModel: UserViewModel
@@ -21,8 +23,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         binding.loginButton.setOnClickListener(this)
     }
@@ -31,9 +31,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val textUsername = binding.username.text.toString()
         val textPassword = binding.password.text.toString()
         val result = userViewModel.userViewModelCheck(textUsername, textPassword)
+
         if (result) {
             Toast.makeText(v.context, "Login Successful!", Toast.LENGTH_LONG).show()
-        } else {
+
+            // Cập nhật trạng thái người dùng
+            userViewModel.updateState(User(textUsername, textPassword))
+
+            // Quan sát stateUser sau khi cập nhật
+            userViewModel.stateUser.observe(this, Observer { newuser ->
+                // Kiểm tra và hiển thị Toast dựa trên trạng thái
+                val message = if (newuser.getGmail().isNotEmpty()) "Updated state" else "Failed"
+                Toast.makeText(v.context, message, Toast.LENGTH_LONG).show()
+            })
+        }else {
             Toast.makeText(v.context, "Login Failed", Toast.LENGTH_LONG).show()
         }
     }
--- app/src/main/java/com/example/loginpage/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/loginpage/viewmodel/UserViewModel.kt
@@ -1,11 +1,20 @@
 package com.example.loginpage.viewmodel
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.loginpage.model.User
 
 class UserViewModel :ViewModel() {
+    private val _stateUser = MutableLiveData<User>()
+    val stateUser: LiveData<User> get() = _stateUser
+
+    fun updateState(newUser: User) {
+        _stateUser.value = newUser
+    }
    fun userViewModelCheck(gmail:String,pass:String):Boolean{
+
         return User(gmail,pass).getGmail().equals("nguoidep433@gmail.com") && User(gmail,pass).getPassword().equals("1234")
    }
 
